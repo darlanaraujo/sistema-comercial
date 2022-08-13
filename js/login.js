4e041582d2fe64fc6606900b9e8afe3bfd2cb3b8
Darlan Araujo <darlanparaujo@outlook.com>
2022-08-13 14:58:00 -0300

    Atualização #3
    Tela de login pronta já com a lógica do JS funcionando. Só falta criar o estilo para mobile.

--- js/login.js
+++ js/login.js
@@ -30,35 +30,91 @@ const setLogin = (nome, email, senha) => {
 
     setBancoLogin(dadosLogin);
 };
-
-const modalAlerta = document.querySelector('#modalAlerta');
-const btnCadastrar = document.querySelector('#btnCadastrar');
-const txtNome = document.querySelector('#txtNome');
-const txtEmail = document.querySelector('#txtEmail');
-const txtSenha = document.querySelector('#txtSenha');
-
 const dadosLogin = getBancoLogin();
 
+// Modal de Alertas
+const alertaErro = document.querySelector('#alertaErro');
+const alertaConfirmacao = document.querySelector('#alertaConfirmacao');
+const txtErro = document.querySelector('#txtErro');
+const txtConfirmacao = document.querySelector('#txtConfirmacao');
+
+// Botões
+const btnCadastrar = document.querySelector('#btnCadastrar');
+const btnEntrar = document.querySelector('#btnEntrar');
+
+// Input de saída de dados
+const txtSetNome = document.querySelector('#txtSetNome');
+const txtSetEmail = document.querySelector('#txtSetEmail');
+const txtSetSenha = document.querySelector('#txtSetSenha');
+
+// Input de entrada de dados
+const txtGetEmail = document.querySelector('#txtGetEmail');
+const txtGetSenha = document.querySelector('#txtGetSenha');
+
+
 const limpaInput = () => {
-    txtNome.value = '';
-    txtEmail.value = '';
-    txtSenha.value = '';
+    txtSetNome.value = '';
+    txtSetEmail.value = '';
+    txtSetSenha.value = '';
+
+    txtGetEmail.value = '';
+    txtGetSenha.value = '';
 };
 
-const verificaCadastro = () => {
-    const nome = txtNome.value;
-    const email = txtEmail.value;
-    const senha = txtSenha.value;
+const msgAlerta = (tipoAlerta, msg) => {
+    if(tipoAlerta === 'erro') {
+        txtErro.innerHTML = msg;
+        alertaErro.classList.add('active');
+        setTimeout(() => {
+            alertaErro.classList.remove('active');
+        }, 2500);
+    } else if(tipoAlerta === 'confirmacao') {
+        txtConfirmacao.innerHTML = msg;
+        alertaConfirmacao.classList.add('active');
+        setTimeout(() => {
+            alertaConfirmacao.classList.remove('active');
+        }, 2500);
+    }
+};
+
+const cadastrarUsuario = () => {
+    const nome = txtSetNome.value;
+    const email = txtSetEmail.value;
+    const senha = txtSetSenha.value;
 
     if(nome === '' || email === '' || senha === '') {
-        modalAlerta.classList.add('active');
+        msgAlerta('erro', 'Preencha todos os dados...');
     } else {
         limpaInput();
+        msgAlerta('confirmacao', 'Cadastro feito com sucesso...');
     
         setLogin(nome, email, senha);
     }
 
 };
-btnCadastrar.addEventListener('click', verificaCadastro);
+btnCadastrar.addEventListener('click', cadastrarUsuario);
 
+const verificaCadastro = () => {
+    const getEmail = txtGetEmail.value;
+    const getSenha = txtGetSenha.value;
+    
+    dadosLogin.forEach((dados) => {
+        let email = dados.email;
+        let senha = dados.senha;
 
+        if (getEmail == '' || getSenha == '') {
+            msgAlerta('erro', 'Preencha todos os dados...');
+        } else {
+            
+            if(getEmail === email && getSenha === senha) {
+                limpaInput()
+                confirmacao = false;
+                msgAlerta('confirmacao', 'Login confirmado com sucesso...');
+            } else {
+                msgAlerta('erro', 'Usuário invalido...');
+            }
+        }
+
+    });
+};
+btnEntrar.addEventListener('click', verificaCadastro)
